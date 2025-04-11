17f367c2ce8192b5ac6b45a129513d3e08e0d27e
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-04-11 16:41:19 -0400

    Add application_pages/confirm_sharpe_ratio.py

---
+++ application_pages/confirm_sharpe_ratio.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+def run_confirm_sharpe_ratio():
+    st.header("Confirm Sharpe Ratio")
+    st.write("This is the confirm sharpe ratio page.")
+
+if __name__ == "__main__":
+    run_confirm_sharpe_ratio()
