b81f00b00880b4602c8154e062290c95ab50a066
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-04-11 11:56:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-
 import streamlit as st
 
-st.set_page_config(page_title="QuCreate Streamlit Lab", layout="wide")
-st.sidebar.image("assets/images/company_logo.jpg")
+st.set_page_config(page_title="QuLab", layout="wide")
+st.sidebar.image("https://www.quantuniversity.com/assets/img/logo5.jpg")
 st.sidebar.divider()
-st.title("QuCreate Streamlit Lab")
+st.title("QuLab")
 st.divider()
 
-# Code goes here
+st.write("Welcome to QuLab - Portfolio Optimization Examples Using Financial Toolbox.")
+st.write("This is a multipage application. Use Streamlit's built-in multipage navigation by clicking on the pages listed on the sidebar.")
 
 st.divider()
 st.write("© 2025 QuantUniversity. All Rights Reserved.")
-st.caption("The purpose of this demonstration is solely for educational use and illustration. "
-           "To access the full legal documentation, please visit this link. Any reproduction of this demonstration "
-           "requires prior written consent from QuantUniversity.")
+st.caption("The purpose of this demonstration is solely for educational use and illustration. Any reproduction of this demonstration requires prior written consent from QuantUniversity.")
