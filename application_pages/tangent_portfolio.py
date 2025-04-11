ae1ad7d4a7ed537f367bb91680a93f3003191a90
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-04-11 16:41:19 -0400

    Add application_pages/tangent_portfolio.py

---
+++ application_pages/tangent_portfolio.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+def run_tangent_portfolio():
+    st.header("Tangent Portfolio")
+    st.write("This is the tangent portfolio page.")
+
+if __name__ == "__main__":
+    run_tangent_portfolio()
