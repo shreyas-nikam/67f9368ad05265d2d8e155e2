751e84a1c37cd83b0a498f6fdc00cead5124ae2a
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-04-11 16:41:16 -0400

    Add application_pages/transaction_costs.py

---
+++ application_pages/transaction_costs.py
@@ -0,0 +1,9 @@
+
+import streamlit as st
+
+def run_transaction_costs():
+    st.header("Transaction Costs")
+    st.write("This is the transaction costs page.")
+
+if __name__ == "__main__":
+    run_transaction_costs()
